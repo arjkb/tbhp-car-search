2cbeb60c4bf828a5568f42c161a1b97af97f8183
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-25 22:59:00 +0530

    add module tbhpscraper.py

---
+++ tbhpscraper.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+
+def search(carname, reviews):
+    for k in reviews:
+        if carname in k.lower():
+            print(k, reviews[k])
+
+def get_soup(link):
+    r = requests.get(link)
+    if r.status_code != requests.codes.ok:
+        return None
+    return BeautifulSoup(r.text, 'html.parser')
+
+def get_reviews():
+    soup = get_soup('https://www.team-bhp.com/forum/official-new-car-reviews/')
+    if soup == None:
+        print("Error fetching page")
+        exit(0)
+
+    navigation_pages = set()
+    navigation_pages.add('https://www.team-bhp.com/forum/official-new-car-reviews/')
+    reviews = dict()
+
+    # get list of all links to be navigated to
+    # go to each of those links, and get links to vehicle reviews
+    for link in soup.find_all('a'):
+        link_title = link.get('title')
+        if(str(link_title).startswith("Show results")):
+            navigation_pages.add(link.get('href'))
+
+    # now go to each page, and get the vehicle review list
+    for page in navigation_pages:
+        soup = get_soup(page)
+        if soup != None:
+            for link in soup.find_all('a'):
+                link_id = link.get('id')
+                if str(link_id).startswith('thread'):
+                    print(link.text, link.get('href'))
+                    reviews[link.text] = link.get('href')
+
+    # print(review)
+    # search("mercedes", review)
+    return reviews
