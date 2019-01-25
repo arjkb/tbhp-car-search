b054de34d1d17a4003ea37d5a02bf0daa52cadc7
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-25 22:59:30 +0530

    load reviews dict from tbhpscraper module

--- search.py
+++ search.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import tbhpscraper
+
 app = Flask(__name__, static_url_path='')
+r = tbhpscraper.get_reviews()
 
 @app.route('/')
 def root():
@@ -8,10 +11,6 @@ def root():
 
 @app.route('/results/')
 def results():
-    r = {'Ford Fiesta (Automatic) : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/111055-ford-fiesta-automatic-test-drive-review.html', 'Fiat Avventura : Test Drive & Review': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/156762-fiat-avventura-test-drive-review.html', '2014 Skoda Yeti Facelift : A Close Look': 'https://www.team-bhp.com/forum/indian-car-scene/155485-2014-skoda-yeti-facelift-close-look.html', 'Mitsubishi Lancer EVO X : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/95116-mitsubishi-lancer-evo-x-test-drive-review.html'}
-    # r = {'a': 'apple', 'b':'ball'}
+    # r = {'Ford Fiesta (Automatic) : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/111055-ford-fiesta-automatic-test-drive-review.html', 'Fiat Avventura : Test Drive & Review': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/156762-fiat-avventura-test-drive-review.html', '2014 Skoda Yeti Facelift : A Close Look': 'https://www.team-bhp.com/forum/indian-car-scene/155485-2014-skoda-yeti-facelift-close-look.html', 'Mitsubishi Lancer EVO X : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/95116-mitsubishi-lancer-evo-x-test-drive-review.html'}
+    # r = tbhpscraper.get_reviews()
     return render_template('index.html', sample_text="Foo", reviews=r)
-
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run()
