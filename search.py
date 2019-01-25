5bc7e36fdf7a2ee228fdebbf9b418acd0ddb07ec
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-25 23:06:53 +0530

    add functionality to search for a particular word (car)

--- search.py
+++ search.py
@@ -14,3 +14,8 @@ def results():
     # r = {'Ford Fiesta (Automatic) : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/111055-ford-fiesta-automatic-test-drive-review.html', 'Fiat Avventura : Test Drive & Review': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/156762-fiat-avventura-test-drive-review.html', '2014 Skoda Yeti Facelift : A Close Look': 'https://www.team-bhp.com/forum/indian-car-scene/155485-2014-skoda-yeti-facelift-close-look.html', 'Mitsubishi Lancer EVO X : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/95116-mitsubishi-lancer-evo-x-test-drive-review.html'}
     # r = tbhpscraper.get_reviews()
     return render_template('index.html', sample_text="Foo", reviews=r)
+
+@app.route('/search/<word>')
+def search(word):
+    r_searched = tbhpscraper.search(word, r)
+    return render_template('index.html', sample_text="Search", reviews=r_searched)
