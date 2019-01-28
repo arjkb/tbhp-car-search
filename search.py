09856e39399b9bd339475ef98e199a3ec3a10fa5
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-28 14:45:03 +0530

    add temmporary dummy data to prevent fetching pages unnecessarily during development

--- search.py
+++ search.py
@@ -7,9 +7,24 @@ baselink_oncr = 'https://www.team-bhp.com/forum/official-new-car-reviews/'
 baselink_tdio = 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/'
 baselink_ltr = 'https://www.team-bhp.com/forum/long-term-ownership-reviews/'
 
-reviews_oncr = tbhpscraper.get_reviews(baselink_oncr)
-reviews_tdio = tbhpscraper.get_reviews(baselink_tdio)
-reviews_ltr = tbhpscraper.get_reviews(baselink_ltr)
+# reviews_oncr = tbhpscraper.get_reviews(baselink_oncr)
+# reviews_tdio = tbhpscraper.get_reviews(baselink_tdio)
+# reviews_ltr = tbhpscraper.get_reviews(baselink_ltr)
+
+reviews_oncr = {
+    'Volkswagen Polo 1.2L GT TSI : Official Review ': 'https://www.team-bhp.com/forum/official-new-car-reviews/135550-volkswagen-polo-1-2l-gt-tsi-official-review.html',
+    'Toyota Innova Crysta': 'https://www.team-bhp.com/forum/official-new-car-reviews/177320-toyota-innova-crysta-official-review.html'
+}
+
+reviews_tdio = {
+    'My W222 Mercedes S-Class (S350 CDI)': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/165052-my-w222-mercedes-s-class-s350-cdi.html',
+    'Driven: Volvo V90 Cross Country': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/190414-driven-volvo-v90-cross-country.html'
+}
+
+reviews_ltr = {
+    'BMW 530d M-Sport (F10) : My pre-worshipped beast ': 'https://www.team-bhp.com/forum/long-term-ownership-reviews/167550-bmw-530d-m-sport-f10-my-pre-worshipped-beast.html',
+    'Hyundai Creta 1.6L CRDi SX(O) - An Ownership Log - Update: 50,000 km up': 'https://www.team-bhp.com/forum/long-term-ownership-reviews/196858-hyundai-creta-1-6l-crdi-sx-o-ownership-log-update-50-000-km-up.html'
+}
 
 @app.route('/')
 def root():
