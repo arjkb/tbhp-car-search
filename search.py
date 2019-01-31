6c5d97dccb358a7615c29dfe0e1971b3d95a0f56
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-31 15:09:14 +0530

    add a route /reviews that shows the reviews of all cars

--- search.py
+++ search.py
@@ -16,8 +16,8 @@ def root():
     # return 'at root'
     return render_template('index.html', sample_text="")
 
-@app.route('/results/')
-def results():
+@app.route('/reviews/')
+def reviews():
     # r = {'Ford Fiesta (Automatic) : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/111055-ford-fiesta-automatic-test-drive-review.html', 'Fiat Avventura : Test Drive & Review': 'https://www.team-bhp.com/forum/test-drives-initial-ownership-reports/156762-fiat-avventura-test-drive-review.html', '2014 Skoda Yeti Facelift : A Close Look': 'https://www.team-bhp.com/forum/indian-car-scene/155485-2014-skoda-yeti-facelift-close-look.html', 'Mitsubishi Lancer EVO X : Test Drive & Review': 'https://www.team-bhp.com/forum/official-new-car-reviews/95116-mitsubishi-lancer-evo-x-test-drive-review.html'}
     # r = tbhpscraper.get_reviews()
     reviews = [
